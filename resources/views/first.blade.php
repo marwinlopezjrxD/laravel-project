ab9d93b85f8d8e76ae513e6dee83980874c7d3b0
Marwin <marwinlopezjr@gmail.com>
2025-01-22 21:14:10 +0800

    updated first.blade.php file

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -1,1 +1,4 @@
-<h1>Hello First File </h1>
+<!-- ?<h1>Hello First File </h1> -->
+
+<h1 style="color: blue;">PLEASE REST MING. YOUVE DONE WELL.</h1>
+<a href="https://www.google.com/search?q=pgsharp&oq=pgsharp&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDExMTZqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8&sei=5-2QZ5TVOvijvr0Ps4LF2Q0" style="text-decoration: none;"><p style="color: green;">play everything u want</p></a>
