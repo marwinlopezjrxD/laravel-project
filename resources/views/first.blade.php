d558f680f9e73ad1938efb02cfa91f1375f85dad
Marwin <marwinlopezjr@gmail.com>
2025-01-23 18:42:21 +0800

    change bg color of <body> tag to dark for less blue light(i think)

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -1,1 +1,11 @@
-<h1>Hello First File(test2)</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel 11 Practice</title>
+</head>
+<body style="background-color: black;">
+    <h1 style="color: white;'">Hello First File</h1>
+</body>
+</html>
