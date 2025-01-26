fd30165bb3d920191a1c1ffe098176e60dbe47a4
Marwin <marwinlopezjr@gmail.com>
2025-01-27 00:44:59 +0800

    uploaded controllerinlaravel file

---
+++ resources/views/controllerinlaravel.blade.php
@@ -0,0 +1,13 @@
+<!--
+ What is controller
+ How to make controller
+ Call controller with routes
+ Pass data from router to controller
+ Call view with controller
+ Call view which is under a nested folder
+ Interview Questions
+ -->
+
+<!-- 
+
+-->
