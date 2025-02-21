ffc46d866f18da39b4993b93e949ee9306b9f1e1
Marwin <marwinlopezjr@gmail.com>
2025-02-21 11:28:06 +0800

    created component
    messageBanner.php
    message-banner.blade.php

---
+++ resources/views/components/message-banner.blade.php
@@ -0,0 +1,4 @@
+<div>
+    {{-- php artisan make:component messageBanner --}}
+    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+</div>
