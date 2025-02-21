b844b2be1fdc8ac9d5dd8b8fa919b3d51eb2d692
Marwin <marwinlopezjr@gmail.com>
2025-02-21 13:20:34 +0800

    components done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
 <x-message-banner msg="user added successfully" class="success"/>
+<x-message-banner msg="user logout successfully" class="success"/>
+<x-message-banner msg="no user found" class="error"/>
+<x-message-banner msg="some users are missing" class="warning"/>
 
 <h1>Home Page</h1>
 
@@ -6,6 +9,22 @@
     .success{
         background-color: lightgreen;
         color: green;
+        padding: 3px 10px;
+        margin: 10px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+    .error{
+        background-color: #ff000082;
+        color: red;
+        padding: 3px 10px;
+        margin: 10px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+    .warning{
+        background-color: #ffa50094;
+        color: orange;
         padding: 3px 10px;
         margin: 10px;
         display: inline-block;
