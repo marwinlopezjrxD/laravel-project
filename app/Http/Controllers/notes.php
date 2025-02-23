3624b0a17c513823ddf4ba1775d5f1d4d0bd86e0
Marwin <marwinlopezjr@gmail.com>
2025-02-23 22:29:51 +0800

    (DONE)Form Validation | input validation | email | max | min | required

--- app/Http/Controllers/notes.php
+++ app/Http/Controllers/notes.php
@@ -132,3 +132,44 @@
 
 //     }
 // }
+
+// Form handling with checkbox, radio button and dropdown
+// namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+
+// class UserController extends Controller
+{
+    // for testing
+    // function addUser(){
+    //     echo "hello from addUser Function";
+    // }
+    //for getting request($request)
+    // function addUser(Request $request){
+    //     return $request;
+    // }
+    // get values one by one
+    // function addUser(Request $request){
+    //     echo $request->city;
+    //     echo "<br>";
+    //     echo $request->gender;
+    //     echo "<br>";
+    //     echo $request->age;
+    //     echo "<br>";
+    //     //error, array to string conversion
+    //     // echo $request->skill;
+    //     //workaround, print_r to show all skills selected
+    //     print_r($request->skill);
+    // }
+    // function addUser(Request $request){
+    //     echo "User name is: $request->username";
+    //     echo "<br>";
+    //     echo "User city is: $request->city";
+    //     echo "<br>";
+    //     echo "User email is: $request->email";
+    //     //other syntax
+    //     echo "<br>";
+    //     echo "User email is: ".$request->input('email');
+
+    // }
+}
--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -182,3 +182,45 @@
         margin: 10px;
     }
 </style> --}}
+
+{{-- Form handling with checkbox, radio button and dropdown --}}
+{{-- <div>
+    <h1>Add New User</h1>
+    <form action="adduser" method="post">
+        @csrf
+        {{-- checkbox type, choose one or multiple, or all --}}
+        {{-- <div>
+            <h4>User Skills</h4>
+            <input type="checkbox" name="skill[]" id="php" value="php">
+            <label for="php" >PHP</label>
+            <input type="checkbox" name="skill[]" id="java" value="Java">
+            <label for="java" >Java</label>
+            <input type="checkbox" name="skill[]" id="node" value="Node">
+            <label for="node" >Node</label>
+        </div>
+        {{-- Radio type, can only choose one--}}
+        {{-- <div>
+            <h4>User Gender</h4>
+            <input type="radio" name="gender" id="male" value="male">
+            <label for="male">Male</label>
+            <input type="radio" name="gender" id="female" value="female">
+            <label for="female">Female</label>
+        </div>
+        {{-- range --}}
+        {{-- <div>
+            <h4>User Age</h4>
+            <input type="range" name="age" id="age" max="100" min="18">
+        </div>
+        {{-- dropdown --}}
+        {{-- <div>
+            <h4>User City</h4>
+            <select name="city">
+                <option value="manila">Manila</option>
+                <option value="makati">Makati</option>
+                <option value="quezon">Quezon</option>
+            </select>
+        </div>
+        {{-- submit form button --}}
+        {{-- <button>Add New User</button>
+    </form>
+</div>  --}}
