eacc740caec9558d0c47bd2d3129998497d3a819
Marwin <marwinlopezjr@gmail.com>
2025-02-23 22:45:39 +0800

    add notes on controller
    path: C:\Users\Marwin Lopez Jr\OneDrive\Desktop\laravel-project\blog\app\Http\Controllers\notes.php
    
    relative path: app\Http\Controllers\notes.php

--- app/Http/Controllers/notes.php
+++ app/Http/Controllers/notes.php
@@ -173,3 +173,25 @@
 
     // }
 }
+
+// -----------------------------------------------------------------------------------------------
+
+// Form Validation | input validation | email | max | min | required
+// namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+
+// class UserController extends Controller
+// {
+//     //
+//     function addUser(Request $request){
+
+//         $request->validate([
+//             'username'=>'required | min:3 | max:15',
+//             'email'=>'required | email',
+//             'city'=>'required',
+//             'skills'=>'required'
+//         ]);
+//         return $request;
+//     }
+// }
